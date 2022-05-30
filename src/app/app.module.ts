53ec83412d4e13ad1333b9d89003c041ef3114fc
Jaimercal <romerocal.jaime@gmail.com>
2022-05-30 17:57:56 +0200

    Register start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -140,6 +140,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { AccessComponent } from './components/access/access.component';
+import { RegistreComponent } from './components/registre/registre.component';
 
 @NgModule({
     imports: [
@@ -273,6 +274,7 @@ import { AccessComponent } from './components/access/access.component';
         ErrorComponent,
         NotfoundComponent,
         AccessComponent,
+        RegistreComponent,
     ],
     providers: [
         {provide: LocationStrategy, useClass: HashLocationStrategy},
