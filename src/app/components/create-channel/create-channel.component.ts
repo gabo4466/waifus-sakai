1236c53eedb7c587334ee54b8b42b266a5c8d482
Gabriel Costanza <costanzag1999@gmail.com>
2022-06-02 12:08:50 +0200

    Channel & CreateChannel Components + routes &dynamic menu

---
+++ src/app/components/create-channel/create-channel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-channel',
+  templateUrl: './create-channel.component.html',
+  styleUrls: ['./create-channel.component.scss']
+})
+export class CreateChannelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
