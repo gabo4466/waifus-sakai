42fdb67a78d56057b3351963e85e9a22d52a9e85
Gabriel Costanza <costanzag1999@gmail.com>
2022-06-02 21:01:44 +0200

    Create channel form

--- src/app/components/create-channel/create-channel.component.ts
+++ src/app/components/create-channel/create-channel.component.ts
@@ -5,6 +5,7 @@ import {Subscription} from "rxjs";
 import {UserService} from "../../service/user.service";
 import {Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {DateService} from "../../service/date.service";
 
 @Component({
   selector: 'app-create-channel',
@@ -16,10 +17,16 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
     config: AppConfig;
     subscription: Subscription;
     fg: FormGroup;
+    today: string;
     constructor( private configService: ConfigService,
                  private userService: UserService,
                  private router: Router,
-                 private fb: FormBuilder) { }
+                 private fb: FormBuilder,
+                 private dateService: DateService) {
+        this.createForm();
+        this.today = dateService.formatDateYYYYMMDD(new Date());
+        console.log(this.today.toString())
+    }
 
     ngOnInit(): void {
         this.config = this.configService.config;
@@ -55,14 +62,23 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
                     Validators.minLength(3),
                     Validators.maxLength(254),
                     Validators.pattern("[a-zA-Z0-9]+")
-                ],
-                []
+                ]
             ],
             description: [
-                "",[],[]
+                "",
+                Validators.required
             ],
             dateChannel: [
-
+                this.today,
+                Validators.required
+            ],
+            photo: [
+                "",
+                [],[]
+            ],
+            banner: [
+                "",
+                [],[]
             ]
         });
     }
@@ -71,6 +87,11 @@ export class CreateChannelComponent implements OnInit, OnDestroy {
         if (this.fg.valid && !this.fg.pending){
 
         }
+    }
+
+    mainPhotoHandler(event){
+        let files = event.currentFiles[0];
+        console.log(files);
     }
 
 }
