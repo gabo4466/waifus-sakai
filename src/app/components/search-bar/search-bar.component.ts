ccbfbcd1674f1e8b6c91eebf882c80e0c09f162e
Gabriel Costanza <costanzag1999@gmail.com>
2022-06-06 19:37:14 +0200

    Search component finished

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-search-bar',
@@ -9,10 +10,24 @@ import {Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class SearchBarComponent implements OnInit {
 
     @Output("term")  termEvent = new EventEmitter<string>();
+    fg: FormGroup;
 
-    constructor() { }
+    constructor( private fb: FormBuilder, ) {
+        this.createForm();
+
+    }
 
     ngOnInit(): void {
+    }
+
+    keyPress(){
+        this.termEvent.emit(this.fg.get("term").value);
+    }
+
+    createForm(){
+        this.fg = this.fb.group({
+            term: [""]
+        });
     }
 
 }
